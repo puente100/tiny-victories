af1e5bc574a37d6abb35a8fc9ecccfa2ba7b036d
DanielPuente <puente10@hotmail.com>
2025-05-02 14:46:57 -0600

    Initial Commit

---
+++ TinyVictories/TinyVictoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyVictoriesApp.swift
+//  TinyVictories
+//
+//  Created by Daniel Puente on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TinyVictoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
